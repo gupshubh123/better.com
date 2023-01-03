72a40ec8c9bf06d4a0abdf80165f93d17deee594
shubham gupta <shubh.gupta0793@gmail.com>
2023-01-03 19:53:56 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [guess, setGuess] = useState("")
+  const [allGuessValues, setallGuessValues]  = useState([])
+  const [actualValue, setActualValue] = useState(null)
+  const ref = useRef(null);
+
+  const guessHandler =(e)=>{
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const guessValue = +data.get('guess')
+    if(guessValue && guessValue>0 && guessValue<=100){
+      setallGuessValues( [...allGuessValues, guessValue])
+      setGuess(guessValue)
+        ref.current.value =""
+        if(guessValue === actualValue){
+          console.log("correct guess")
+          setActualValue(null)
+          setallGuessValues([])
+        }
+    }
+  }
+
+  const startGame =() =>{
+    const actualVal = Math.ceil(Math.random()*100)
+    console.log(actualVal)
+    setActualValue(actualVal)
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
